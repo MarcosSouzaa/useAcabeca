004356970e266dc391851b1ecdd91681002f42e2
Marcos <m.storeproducoes@gmail.com>
2022-05-21 17:04:04 -0300

    primeiro commit

---
+++ js/exe1.js
@@ -0,0 +1,13 @@
+//Enquanto colheres forem maior que 5 repe a frase "Outras Colheres"
+//decrementa 1
+// até que o valor de scoops seja 0. Então sai do loop e exibe a frase:
+// Vida sem sorvete não é a mesma!
+console.log(testeeeee)
+
+        var scoops = 5
+        while (scoops > 0) {
+            document.write("Another scoop!<br>")
+            scoops = scoops - 1             
+             }        
+     document.write("<br><h1>life without ice cream isn't the same!</h1>")
+     
